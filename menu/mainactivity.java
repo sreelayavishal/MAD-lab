35bf745d18312eb38ae01b3551ab94db8c90f07e
Sreelaya Vishal <93375670+sreelayavishal@users.noreply.github.com>
2022-10-12 11:53:25 +0530

    Create mainactivity.java

---
+++ menu/mainactivity.java
@@ -0,0 +1,58 @@
+package com.example.mymenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean OnCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id= item.getItemId();
+        if(id==R.id.menu_home)
+        {
+            Toast.makeText(this, "clicked option is home", Toast.LENGTH_SHORT).show();
+        }
+        if(id==R.id.menu_about)
+        {
+            Toast.makeText(this, "clicked option is about", Toast.LENGTH_SHORT).show();
+        }
+        if(id==R.id.menu_contact)
+        {
+            Toast.makeText(this, "clicked option is contact", Toast.LENGTH_SHORT).show();
+        }
+        if(id==R.id.menu_setting)
+        {
+            Toast.makeText(this, "clicked option is settings", Toast.LENGTH_SHORT).show();
+        }
+        if(id==R.id.menu_share)
+        {
+            Toast.makeText(this, "clicked option is home", Toast.LENGTH_SHORT).show();
+            String url="https://www.google.com/".toString();
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
